d67dfc8b80ff2a111527b9f8cf8c00ffb6c7c959
Chris Laskey <contact@chrislaskey.com>
2017-07-18 11:22:05 -0400

    Add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Ink.User
+alias Ink.Post
+alias Ink.Repo
+
+Repo.insert!(%User{name: "Chris Laskey", email: "contact@chrislaskey.com"})
+Repo.insert!(%User{name: "Denali", email: "denali@chrislaskey.com"})
+
+Enum.map(1..10, fn(_) ->
+  Ink.Repo.insert!(%Post{
+    title: Faker.Lorem.sentence,
+    body: Faker.Lorem.paragraph,
+    user_id: [1, 2] |> Enum.take_random(1) |> hd
+  })
+end)
+
+### Alternative way using a Module to organize code:
+
+# defmodule Ink.Seeds do
+#   def generate_posts() do
+#     Ink.Repo.insert!(%Post{
+#       title: Faker.Lorem.sentence,
+#       body: Faker.Lorem.paragraph,
+#       user_id: [1, 2] |> Enum.take_random(1) |> hd
+#     })
+#   end
+# end
+
+# Long form:
+# Enum.map(1..10, fn(_) -> Ink.Seeds.generate_posts(_) end)
+
+# Short form (using & syntax):
+# Enum.map(1..10, &Ink.Seeds.generate_posts(&1))
+
+### Alternative way using `for`:
+
+# for _ <- 1..10 do
+#   Repo.insert!(%Post{})
+# end
--- web/models/user.ex
+++ web/models/user.ex
@@ -4,6 +4,7 @@ defmodule Ink.User do
   schema "users" do
     field :name, :string
     field :email, :string
+    has_many :posts, Ink.Post
 
     timestamps()
   end
