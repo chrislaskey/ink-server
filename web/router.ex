747e5685029e19d35c565845cc20a7d9915a22f1
Chris Laskey <contact@chrislaskey.com>
2017-07-20 10:50:49 -0400

    Add graphql plug

--- web/router.ex
+++ web/router.ex
@@ -13,14 +13,24 @@ defmodule Ink.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :graphql do
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+    plug Ink.Web.Context
+  end
+
   scope "/", Ink do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
   end
 
-  forward "/api", Absinthe.Plug,
-    schema: Ink.Schema
+  scope "/api" do
+    pipe_through :graphql
+
+    forward "/", Absinthe.Plug,
+      schema: Ink.Schema
+  end
 
   if Application.get_env(:ink, :graphiql, false), do:
     forward "/graphiql", Absinthe.Plug.GraphiQL,
--- web/schema.ex
+++ web/schema.ex
@@ -30,6 +30,12 @@ defmodule Ink.Schema do
     field :user_id, non_null(:integer)
   end
 
+  input_object :update_user_params do
+    field :name, :string
+    field :email, :string
+    field :password, :string
+  end
+
   mutation do
     field :create_post, type: :post do
       arg :title, non_null(:string)
@@ -49,6 +55,13 @@ defmodule Ink.Schema do
     field :delete_post, type: :post do
       arg :id, non_null(:integer)
       resolve &Resolver.Post.delete/2
+    end
+
+    field :update_user, type: :user do
+      arg :id, non_null(:integer)
+      arg :user, :update_user_params
+
+      resolve &Resolver.User.update/2
     end
   end
 end
