380d8f0129ad2c40037b4f81e9c21e3327fb2ba1
Chris Laskey <contact@chrislaskey.com>
2017-08-12 10:40:13 -0400

    Blocking out Facebook OAuth2 access token

--- config/config.exs
+++ config/config.exs
@@ -32,6 +32,10 @@ config :guardian, Guardian,
   secret_key: System.get_env("GUARDIAN_SECRET_KEY"),
   serializer: Ink.Guardian.Serializer
 
+config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
+  client_id: System.get_env("FACEBOOK_APP_ID"),
+  client_secret: System.get_env("FACEBOOK_APP_SECRET")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
--- lib/ink/resolvers/user.ex
+++ lib/ink/resolvers/user.ex
@@ -1,4 +1,6 @@
 defmodule Ink.Resolver.User do
+  require Logger
+
   alias Ink.Repo
   alias Ink.CurrentUser
   alias Ink.User
@@ -17,6 +19,46 @@ defmodule Ink.Resolver.User do
     with {:ok, user} <- Session.authenticate(params, Repo),
          {:ok, jwt, %{"exp" => expires}} <- Guardian.encode_and_sign(user, :access) do
       {:ok, %{token: jwt, token_expiration: expires, user: user}}
+    end
+  end
+
+  def log_in_with_provider(params, _info) do
+    with {:ok, token} <- get_provider_token(params) do
+      IO.puts "access_token"
+      IO.puts access_token
+
+      Logger.debug "Access token: " <> inspect(access_token)
+
+      # token.refresh_token
+      user = hd(Repo.all(User))
+
+      {:ok, %{token: token.access_token, token_expiration: token.expires_at, user: user}}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  defp get_provider_token(%{code: code, provider: provider, redirect_uri: redirect_uri}) do
+    case provider do
+      "facebook" -> get_facebook_token(code, redirect_uri)
+      _ -> {:error, "Provider #{provider} not supported"}
+    end
+  end
+
+  defp get_facebook_token(code, redirect_uri) do
+    try do
+      client = Ueberauth.Strategy.Facebook.OAuth.get_token!([code: code], [redirect_uri: redirect_uri])
+      token = client.token
+
+      Logger.debug "Facebook client: " <> inspect(client)
+
+      case token.access_token do
+        nil -> {:error, "Error fetching facebook token: " <> token.other_params["error_description"]}
+        _ -> {:ok, token}
+      end
+    rescue
+      error in ArgumentError -> Logger.error inspect(error)
+        {:error, "Error fetching facebook token"}
     end
   end
 end
