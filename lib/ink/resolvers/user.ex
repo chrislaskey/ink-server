ee5e5f7420815f7f540c4932d5c80f8caff43cc8
Chris Laskey <contact@chrislaskey.com>
2017-08-15 08:48:20 -0400

    Translate facebook data to user map

--- lib/ink/resolvers/user.ex
+++ lib/ink/resolvers/user.ex
@@ -3,8 +3,9 @@ defmodule Ink.Resolver.User do
 
   alias Ink.Repo
   alias Ink.CurrentUser
-  alias Ink.User
   alias Ink.Session
+  alias Ink.User
+  alias Ink.User.Instance, as: UserInstance
 
   def update(%{id: id, user: user_params}, info) do
     case id == CurrentUser.id(info) do
@@ -25,42 +26,52 @@ defmodule Ink.Resolver.User do
   end
 
   def log_in_with_provider(params, _info) do
-    with {:ok, token} <- get_provider_token(params) do
-      IO.puts "access_token"
-      IO.puts access_token
-
-      Logger.debug "Access token: " <> inspect(access_token)
-
-      # token.refresh_token
-      user = hd(Repo.all(User))
-
-      {:ok, %{token: token.access_token, token_expiration: token.expires_at, user: user}}
+    with {:ok, user_data} <- get_provider_user(params),
+         {:ok, user} <- UserInstance.find_or_create_by(user_data),
+         {:ok, jwt, %{"exp" => expires}} <- Guardian.encode_and_sign(user, :access) do
+      {:ok, %{token: jwt, token_expiration: expires, user: user}}
     else
       {:error, reason} -> {:error, reason}
     end
   end
 
-  defp get_provider_token(%{code: code, provider: provider, redirect_uri: redirect_uri}) do
+  defp get_provider_user(%{code: code, provider: provider, redirect_uri: redirect_uri}) do
     case provider do
-      "facebook" -> get_facebook_token(code, redirect_uri)
+      "facebook" -> get_facebook_user(code, redirect_uri)
       _ -> {:error, "Provider #{provider} not supported"}
     end
   end
 
-  defp get_facebook_token(code, redirect_uri) do
+  defp get_facebook_user(code, redirect_uri) do
     try do
-      client = Ueberauth.Strategy.Facebook.OAuth.get_token!([code: code], [redirect_uri: redirect_uri])
+      client = OAuth2.Provider.Facebook.get_token!([code: code], [redirect_uri: redirect_uri])
       token = client.token
 
       Logger.debug "Facebook client: " <> inspect(client)
 
       case token.access_token do
         nil -> {:error, "Error fetching facebook token: " <> token.other_params["error_description"]}
-        _ -> {:ok, token}
+        _ -> get_facebook_user_details(client)
       end
     rescue
       error in ArgumentError -> Logger.error inspect(error)
         {:error, "Error fetching facebook token"}
+    end
+  end
+
+  defp get_facebook_user_details(client) do
+    case OAuth2.Provider.Facebook.get_user(client) do
+      {:error, response} -> {:error, response}
+      {:ok, user} ->
+        {:ok, %{
+          provider_name: :facebook,
+          provider_id: user["id"],
+          locale: user["locale"],
+          email: user["email"],
+          first_name: user["first_name"],
+          last_name: user["last_name"],
+          name: user["name"]
+        }}
     end
   end
 end
