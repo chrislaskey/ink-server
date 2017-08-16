8f1a9945c971f582875f6ecc610c447bf8ecf931
Chris Laskey <contact@chrislaskey.com>
2017-08-15 20:13:55 -0400

    Break facebook log in code into separate module

--- lib/ink/resolvers/user.ex
+++ lib/ink/resolvers/user.ex
@@ -5,6 +5,9 @@ defmodule Ink.Resolver.User do
   alias Ink.CurrentUser
   alias Ink.Session
   alias Ink.User
+
+  alias Ink.LogIn.Facebook
+
   alias Ink.User.Instance, as: UserInstance
   alias Ink.UserProvider.Instance, as: UserProviderInstance
 
@@ -40,53 +43,8 @@ defmodule Ink.Resolver.User do
 
   defp get_provider_data(%{code: code, provider: provider, redirect_uri: redirect_uri}) do
     case provider do
-      "facebook" -> get_facebook_data(code, redirect_uri)
+      "facebook" -> Facebook.fetch(code, redirect_uri)
       _ -> {:error, "Provider #{provider} not supported"}
     end
-  end
-
-  defp get_facebook_data(code, redirect_uri) do
-    try do
-      client = OAuth2.Provider.Facebook.get_token!([code: code], [redirect_uri: redirect_uri])
-      token = client.token
-      Logger.debug "Facebook client: " <> inspect(client)
-
-      case token.access_token do
-        nil -> {:error, "Error fetching facebook token: " <> token.other_params["error_description"]}
-        _ -> get_facebook_user(client)
-      end
-    rescue
-      error in ArgumentError -> Logger.error inspect(error)
-        {:error, "Error fetching facebook token"}
-    end
-  end
-
-  defp get_facebook_user(client) do
-    case OAuth2.Provider.Facebook.get_user(client) do
-      {:error, response} -> {:error, response}
-      {:ok, user} ->
-        Logger.debug "Facebook user: " <> inspect(user)
-        {:ok, facebook_user(client, user)}
-    end
-  end
-
-  defp facebook_user(client, user) do
-    %{
-      provider: %{
-        type: "facebook",
-        provider_id: user["id"],
-        client_id: client.client_id,
-        access_token: client.token.access_token,
-        expires_at: client.token.expires_at,
-        refresh_token: client.token.refresh_token
-      },
-      user: %{
-        locale: user["locale"],
-        email: user["email"],
-        first_name: user["first_name"],
-        last_name: user["last_name"],
-        name: user["name"]
-      }
-    }
   end
 end
