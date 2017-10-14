d5de60ceff978e3233f30a46e275831d003ccde2
Chris Laskey <contact@chrislaskey.com>
2017-10-14 11:51:13 -0400

    Remove socket from InkApi

--- apps/ink_api/lib/ink_api/endpoint.ex
+++ apps/ink_api/lib/ink_api/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule InkApi.Endpoint do
   use Phoenix.Endpoint, otp_app: :ink_api
 
-  socket "/socket", InkApi.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
